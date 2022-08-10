cc8527b3c68558b55e2a84d859e30bba25faff94
Norinot <naruto010190@gmail.com>
2022-08-10 17:52:20 +0200

    Remove, Adding items fix.

--- todo-app/src/app/todo-list/todo-list.component.ts
+++ todo-app/src/app/todo-list/todo-list.component.ts
@@ -7,18 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
   items: string[];
-  itemsCount: number;
+  itemsCounter: number;
 
   constructor() {
     this.items = [];
-    this.itemsCount = this.items.length;
+    this.itemsCounter = this.items.length;
   }
 
   ngOnInit(): void {}
 
+  get itemsCount():number {
+    return this.items.length;
+  }
+
   onAddToDo() {
-    this.items.push('Todo' + (this.itemsCount + 1));
-    this.itemsCount = this.items.length;
+    this.items.push('Todo' + (this.itemsCounter + 1));
+    this.itemsCounter++;
   }
 
   onRemoveToDo() {
@@ -28,8 +32,9 @@ export class TodoListComponent implements OnInit {
   onRemoveItem(item: string){
     const id = this.items.indexOf(item);
     this.items.splice(id, 1);
-    this.itemsCount--;
+    console.log(this.itemsCounter);
   }
+
 }
 
 
