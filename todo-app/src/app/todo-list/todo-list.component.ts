b22981b68541caedeb856914df38c0957399c315
funkstermonster <fanni.szasz96@gmail.com>
2022-08-10 16:35:18 +0200

    end of the day

---
+++ todo-app/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  _itemsCount:number;
+  items:string[];
+
+  constructor() {
+    this.items = ["Todo1", "Todo2", "Todo3", "Todo4", "Todo5"];
+    this._itemsCount = this.items.length;
+  }
+
+  get itemsCount():number {
+    return this._itemsCount;
+  }
+
+  ngOnInit(): void {
+  }
+
+  addItem(): void {
+    this.items.push("Todo" + (this.itemsCount + 1));
+    this._itemsCount += 1;
+  }
+
+  removeTopItem(): void {
+    this.items.shift();
+  }
+
+  removeItem(item: string): void {
+    this.items = this.items.filter(i => i !== item);
+  }
+}
