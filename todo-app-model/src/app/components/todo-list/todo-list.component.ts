8261fe578344dfe90c0dfc8b5cb0acac9c031456
markitoo3 <dietrich.marko@gmail.com>
2022-08-11 13:47:37 +0200

    Add simple (not working) todo list

---
+++ todo-app-model/src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  items: Todo[] = new Array();
+
+  constructor() {
+    this.items.push({id: 1, title: 'Ezmegaz', createDate: '2022-08-11'});
+    this.items.push({id: 2, title: 'Megamaz', createDate: '2022-08-05'});
+    this.items.push({id: 3, title: 'Ésezis', createDate: '2022-07-21'});
+  }
+
+  ngOnInit(): void {
+  }
+
+}
