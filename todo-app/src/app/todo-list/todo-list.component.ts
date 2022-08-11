8ef45aaff65dd4d63b6df3a992ba7323cc4f8ae7
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2022-08-11 12:34:51 +0200

    Grey rows, Too many items, Prime rows

--- todo-app/src/app/todo-list/todo-list.component.ts
+++ todo-app/src/app/todo-list/todo-list.component.ts
@@ -3,46 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-
-  nextId:number;
-  items:string[];
+  nextId: number;
+  items: string[];
 
   constructor() {
-    this.items = ["Todo1", "Todo2", "Todo3", "Todo4", "Todo5"];
+    this.items = ['Todo1', 'Todo2', 'Todo3', 'Todo4', 'Todo5'];
     this.nextId = this.items.length + 1;
   }
 
-  get itemsCount():number {
+  get itemsCount(): number {
     return this.items.length;
   }
 
-  ngOnInit(): void {
+  get isTooManyTodos(): boolean {
+    return this.items.length > 8;
   }
 
+  ngOnInit(): void {}
+
   addItem(): void {
-    this.items.push("Todo" + this.nextId);
+    this.items.push('Todo' + this.nextId);
     this.nextId += 1;
   }
-  
+
   addItem2(): void {
     let maxId = 0;
-    this.items.forEach(i => {
-      let todoId = parseInt(i.split("Todo")[1]);
+    this.items.forEach((i) => {
+      let todoId = parseInt(i.split('Todo')[1]);
       maxId = todoId > maxId ? todoId : maxId;
-    })
-    this.items.push("Todo" + (maxId + 1));
+    });
+    this.items.push('Todo' + (maxId + 1));
     this.nextId += 1;
   }
-  
+
+  isPrime(num: number): boolean {
+    if (num === 0 || num === 1 ) return false;
+    if (num === 2 ) return true;
+    let maxCheck = Math.ceil(Math.sqrt(num));
+    for (let i = 2; i <= maxCheck; i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+
   removeTopItem(): void {
     this.items.shift();
   }
 
   removeItem(item: string): void {
-    this.items = this.items.filter(i => i !== item);
+    this.items = this.items.filter((i) => i !== item);
   }
-
 }
