f86cecdfef1905bf2b565cef84d18461896c6225
Freetze <babos1221@gmail.com>
2022-08-11 15:44:25 +0200

    Freetze Angular

---
+++ todo-app-model/todo-app-model/src/app/components/todo-list-multi/todo-list-multi.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-list-multi',
+  templateUrl: './todo-list-multi.component.html',
+  styleUrls: ['./todo-list-multi.component.css']
+})
+export class TodoListMultiComponent implements OnInit {
+  items: Todo[] = new Array();
+
+  constructor() {
+    this.items = [
+      {id: 1, title: 'Write an email', createDate:'2022-08-11'},
+      {id: 2, title: 'Make an invoice', createDate:'2022-08-05', subTodos: [
+        {id: 4, title:'Check preview', createDate: '2022-08-11'},
+        {id: 5, title:'Check preview', createDate: '2022-08-11'},
+      ]},
+      {id: 3, title: 'Paint a painting', createDate:'2022-07-21'},
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
