bf22c5ef62e23335b16eed789bd25df65009b64f
ettella <ettella@hotmail.com>
2022-08-17 13:29:06 +0200

    "Form-templates"

---
+++ Forms/src/app/form-template/form-template.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-template',
+  templateUrl: './form-template.component.html',
+  styleUrls: ['./form-template.component.css']
+})
+export class FormTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(formValue: string): void{
+    console.log('Formvalue',formValue);
+  }
+
+}
