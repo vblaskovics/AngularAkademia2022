99887106216b9124ebf22c1e662ddafab2defb13
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2022-08-17 15:12:13 +0200

    Add reactive forms

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormTemplateComponent } from './form-template/form-template.component';
+import { FormReactiveComponent } from './form-reactive/form-reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormTemplateComponent
+    FormTemplateComponent,
+    FormReactiveComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
