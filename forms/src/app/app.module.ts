07b18828af0b7f6cf96e9fda5e5a86723bce7bea
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2022-08-17 13:28:04 +0200

    Add form-template component

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormTemplateComponent } from './form-template/form-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormTemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
