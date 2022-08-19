a462f30b11e23126c19bb8f2cd3328daa144464e
Freetze <babos1221@gmail.com>
2022-08-19 22:03:01 +0200

    login screen template driven

--- todo-manager-webapp/src/app/login-todo/login-todo.component.ts
+++ todo-manager-webapp/src/app/login-todo/login-todo.component.ts
@@ -6,9 +6,12 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   templateUrl: './login-todo.component.html',
   styleUrls: ['./login-todo.component.css']
 })
+
 export class LoginTodoComponent implements OnInit {
 
+  log(x: any) { console.log(x); }
 
+  submit(f: any) {console.log(f);}
 
   constructor() { }
 
