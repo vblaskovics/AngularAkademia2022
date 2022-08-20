c1863cfbb310efd3823084754d69ceeab79c0c43
Szász <Fanni.Szasz@t-systems.com>
2022-08-20 17:37:53 +0200

    form without validations

--- todo-manager/src/app/components/sign-in-form/sign-in-form.component.ts
+++ todo-manager/src/app/components/sign-in-form/sign-in-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in-form',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in-form.component.css']
 })
 export class SignInFormComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      username: ['', [Validators.minLength(4), Validators.pattern(/a-z0-9/)]], //lowercase and numbers only
+      zip: ['', [Validators.minLength(1000), Validators.maxLength(9999)]],
+      password: ['', [Validators.minLength(8), Validators.pattern(/(?=.*[@.*#])/)]] //must contains at least one of the following: ., #, @, *
+    })
+  }
 
   ngOnInit(): void {
   }
