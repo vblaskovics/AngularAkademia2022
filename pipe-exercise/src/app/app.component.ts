50bb53029ead6570027416e28573ec8aaad4ece6
Freetze <babos1221@gmail.com>
2022-08-24 17:05:25 +0200

    Pipe modifications

--- pipe-exercise/src/app/app.component.ts
+++ pipe-exercise/src/app/app.component.ts
@@ -10,5 +10,11 @@ export class AppComponent {
   currentDate = Date.now();
 
 
-  itemArray = [{ fileName: 'tököm', date: Date, size: 'mb', item: '.exe' }];
+  itemArray = [
+    { fileName: 'letter.docx', modifyDate: 1641254400000, size: 233212458 },
+    { fileName: 'letter.v02.docx', modifyDate: new Date('2022-04-23').getTime(), size: 2333016458 },
+    { fileName: 'letter.v04.docx', modifyDate: new Date('2022-07-30').getTime(), size: 5125515682 },
+    { fileName: 'letter.v07.docx', modifyDate: new Date('2022-02-22').getTime(), size: 6847625123 },
+
+  ];
 }
--- pipe-exercise/src/app/pipes.pipe.ts
+++ pipe-exercise/src/app/pipes.pipe.ts
@@ -1,12 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'pipes'
+  name: 'fileSize',
 })
 export class PipesPipe implements PipeTransform {
+  private units = ['bytes', 'KB', 'MB', 'GB'];
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(bytes: number = 0, precision: number = 2): string {
+    if (isNaN(parseFloat(String(bytes))) || !isFinite(bytes)) {
+      return '?';
+    }
+
+    let unit = 0;
+
+    while (bytes >= 1024) {
+      bytes /= 1024;
+
+      unit++;
+    }
+
+    return bytes.toFixed(+precision) + ' ' + this.units[unit];
   }
-
 }
