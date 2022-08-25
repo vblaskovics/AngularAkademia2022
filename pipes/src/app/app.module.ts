9816050ad6c4fc1ccbd972147ba2417d39549a6b
Szász <Fanni.Szasz@t-systems.com>
2022-08-25 08:55:48 +0200

    pipes practice

---
+++ pipes/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PipePageComponent } from './pipe-page/pipe-page.component';
+import { HelloPipe } from './pipes/hello.pipe';
+import { PipeCustomComponent } from './pipe-custom/pipe-custom.component';
+import { OrderPipe } from './pipes/order.pipe';
+import { CounterPipe } from './pipes/counter.pipe';
+import { FilesComponent } from './files/files.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PipePageComponent,
+    HelloPipe,
+    PipeCustomComponent,
+    OrderPipe,
+    CounterPipe,
+    FilesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ pipes/src/app/pipes/order.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'order'
+})
+export class OrderPipe implements PipeTransform {
+
+  transform(input:string, separator:string=" "): string {
+    return input.split(" ").reverse().join(" ");
+  }
+
+}
