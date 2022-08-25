b24fa1fa266ab66b672a26dcb0f8ee6360e36353
BeZol22 <zoltan.beke89@gmail.com>
2022-08-25 14:20:45 +0200

    Fix
    files size working with button + pipes

--- pipes/src/app/file-table/file-table.component.ts
+++ pipes/src/app/file-table/file-table.component.ts
@@ -8,41 +8,30 @@ import { Files } from '../models/file-model';
 })
 export class FileTableComponent implements OnInit {
   files: Files[] = new Array();
-  mbStatus: boolean = false;
-  gbStatus: boolean = false;
+  unit = 'MB';
 
   constructor() {
-    this.mbStatus = false;
-    this.gbStatus = false;
     this.files = [
       {
         id: 1,
         name: 'run.home.exe',
         modifyDate: new Date(2021, 2, 21, 14, 22).getTime(),
-        size: 4324323,
+        size: 432224323,
       },
       {
         id: 2,
         name: 'image.jpg',
         modifyDate: new Date(2021, 3, 2, 10, 2).getTime(),
-        size: 34323,
+        size: 3422323,
       },
       {
         id: 3,
         name: 'document.pdf',
         modifyDate: new Date(2022, 6, 19, 3, 22).getTime(),
-        size: 12434323,
+        size: 1222434323,
       },
     ];
   }
 
   ngOnInit(): void {}
-
-  fileToMbClicked() {
-    return !this.mbStatus;
-  }
-
-  fileToGbClicked() {
-    return !this.gbStatus;
-  }
 }
--- pipes/src/app/pipes/file-size.pipe.ts
+++ pipes/src/app/pipes/file-size.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'fileSize'
+  name: 'fileSize',
 })
 export class FileSizePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, type: string = 'MB'): string {
+    if (type === 'MB') {
+      return `${Math.floor((value / 1048576) * 100) / 100} MB`;
+    } else if (type === 'GB') {
+      return `${Math.floor((value / 1073741824) * 100) / 100} GB`;
+    }
+    return `${value}`;
   }
-
 }
