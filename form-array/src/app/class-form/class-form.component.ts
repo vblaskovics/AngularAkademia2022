2af4c3da454936868c66cfb5809aa3d587e277ff
ettella <ettella@hotmail.com>
2022-08-30 19:23:37 +0200

    maxNumValidator in class-form comp. - DONE

--- form-array/src/app/class-form/class-form.component.ts
+++ form-array/src/app/class-form/class-form.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
-function maxNumValidator(group: AbstractControl): {[s:string]: boolean} {
-  const maxNumberofstudents = group.get('maxNumberOfStudents')?.value;
-  const students = group.get('students')?.value;
-  const studentNumber = students.length === 30
+// function maxNumValidator(group: AbstractControl): {[s:string]: boolean} {
+//   const maxNumberofstudents = group.get('maxNumberOfStudents')?.value;
+//   const students = group.get('students')?.value;
+//   const studentNumber = students.length === 30
 
-  if ( maxNumberofstudents > studentNumber) {
-    return {errorByMaxNum: true}
+//   if ( maxNumberofstudents > studentNumber) {
+//     return {errorByMaxNum: true}
+//   }
+//   return {}
+//   // if(maxNumberofstudents > 3)
+//   // return {errorByMaxNum: true}
+//   // return {}
+// }
+
+export const maxLengthArray = (max: number) => {
+  return (c: AbstractControl): {[key: string]: any } | null => {
+    if (c.value.length <= max)
+      return null;
+
+    return { MaxLengthArray: true};
   }
-  return {}
-  // if(maxNumberofstudents > 3)
-  // return {errorByMaxNum: true}
-  // return {}
+}
+
+export const minLengthArray = (min: number) => {
+  return (c: AbstractControl): {[key: string]: any } | null => {
+    if (c.value.length >= min)
+      return null;
+
+    return { MinLengthArray: true};
+  }
 }
 
 @Component({
@@ -27,12 +45,8 @@ export class ClassFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
       className: ['', Validators.required],
-      maxNumberOfStudents: ['', 
-      Validators.required,
-      // Validators.max(30)
-    ],
-      students: fb.array([]),
-    }, {validators: [maxNumValidator]});
+      students: fb.array([], [minLengthArray(1), maxLengthArray(3)]),
+    });
    }
 
   get students(): FormArray {
@@ -47,6 +61,10 @@ export class ClassFormComponent implements OnInit {
     return this.form.get('maxNumberOfStudents') as FormControl;
   }
 
+  get numberOfStudents(): number {
+    return this.form.get('students')?.value?.length
+  }
+
   getStudents(students: AbstractControl): FormGroup{
     return students as FormGroup;
   }
@@ -54,17 +72,24 @@ export class ClassFormComponent implements OnInit {
   ngOnInit(): void {}
 
   
-  addStudent(){
+  newStudent(){ //addStudent volt korábban
     const studentFormGroup : FormGroup = this.fb.group({
       name: ['', Validators.required],
       gender: ['', Validators.required],
-      email: ['', Validators.required]
+      email: ['',Validators.required]
     });
     this.students.push(studentFormGroup);
+
+    console.log(studentFormGroup.get('gender')?.hasError);
+    
   }
 
   deleteStudent(studentIndex: number){
     this.students.removeAt(studentIndex);
+  }
+
+  addStudent(){
+
   }
 
 
@@ -73,4 +98,8 @@ export class ClassFormComponent implements OnInit {
     console.log('Submitted form: ',this.form.value)
   }
 
+
+
+
+  
 }
