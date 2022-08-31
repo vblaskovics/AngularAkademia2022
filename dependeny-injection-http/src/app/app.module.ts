a2f5a75225894930492e5ce2fce270ca69b00b4f
ettella <ettella@hotmail.com>
2022-08-31 11:30:10 +0200

    using mockaroo for creating users

--- dependeny-injection-http/src/app/app.module.ts
+++ dependeny-injection-http/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicFormComponent } from './components/basic-form/basic-form.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
