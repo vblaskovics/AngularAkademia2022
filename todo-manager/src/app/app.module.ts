9310b13705683a33630abc0389601b3dcc9a45df
zsofip <91950899+zsofip@users.noreply.github.com>
2022-09-07 13:05:39 +0200

    routing added

--- todo-manager/src/app/app.module.ts
+++ todo-manager/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoTableComponent } from './components/todo-table/todo-table.component';
 import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const routes: Routes = [
+  {path: 'todo-table', component: TodoTableComponent},
+  {path: 'sign-in', component: SignInComponent},
+]
 
 @NgModule({
   declarations: [
@@ -19,7 +26,8 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
