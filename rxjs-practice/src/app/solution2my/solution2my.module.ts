c0cd324e5e5fc966949190ab30b671f5750d7143
markitoo3 <dietrich.marko@gmail.com>
2022-09-08 14:11:26 +0200

    atya úr isten

---
+++ rxjs-practice/src/app/solution2my/solution2my.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SolutionComponent } from './solution.component';
+
+
+
+@NgModule({
+  declarations: [
+    SolutionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Solution2myModule { }
