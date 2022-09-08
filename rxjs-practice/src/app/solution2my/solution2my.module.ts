c8d1336ba71e45dbc14a7eda75336b804ee66732
floratoth <a.flora95@gmail.com>
2022-09-08 14:32:22 +0200

    Add solution2my module with solution1 functionality

---
+++ rxjs-practice/src/app/solution2my/solution2my.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SolutionComponent } from './solution.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { DisplayComponent } from './components/display/display.component';
+import { ScoreComponent } from './components/score/score.component';
+import { TypingFieldComponent } from './components/typing-field/typing-field.component';
+import { FormsModule } from '@angular/forms';
+import { StateService } from './services/state.service';
+
+@NgModule({
+  declarations: [
+    SolutionComponent,
+    CounterComponent,
+    DisplayComponent,
+    ScoreComponent,
+    TypingFieldComponent,
+  ],
+  imports: [CommonModule, FormsModule],
+  exports: [SolutionComponent],
+  providers: [StateService],
+})
+export class Solution2myModule {}
