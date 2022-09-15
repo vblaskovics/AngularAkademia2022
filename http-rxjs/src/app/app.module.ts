388413460a705d02bd53080d96ac75c009302e97
vblaskovics <viktor.blaskovics@gmail.com>
2022-09-15 10:37:08 +0200

    http-rxjs 1.feladat done

--- http-rxjs/src/app/app.module.ts
+++ http-rxjs/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '1_feladat', pathMatch: 'full'},
+  { path: '1_feladat', component: UserListComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ http-rxjs/src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users!:Observable<User[]>;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+  }
+
+}
