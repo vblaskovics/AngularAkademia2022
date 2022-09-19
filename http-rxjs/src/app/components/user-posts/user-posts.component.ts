dc21b5d15338c4022852dfbcc54faf2d78c6ee69
BeZol22 <zoltan.beke89@gmail.com>
2022-09-19 12:50:58 +0200

    http-rxjs lesson 5. forkjoin()

--- http-rxjs/src/app/components/user-posts/user-posts.component.ts
+++ http-rxjs/src/app/components/user-posts/user-posts.component.ts
@@ -1,29 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, forkJoin, delay } from 'rxjs';
+import { Post } from 'src/app/models/post';
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-posts',
   templateUrl: './user-posts.component.html',
-  styleUrls: ['./user-posts.component.css']
+  styleUrls: ['./user-posts.component.css'],
 })
 export class UserPostsComponent implements OnInit {
+  usersWithPostCount$: Observable<any[]> = new Observable<any[]>();
 
-  usersWithPostCount$:Observable<any[]> = new Observable();
-  constructor(private userService:UserService, private postService:PostService) { }
+  user1?: { name: string; count: number };
+  user2?: { name: string; count: number };
+  user3?: { name: string; count: number };
+
+  constructor(
+    private userService: UserService,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
-    /*this.usersWithPostCount$ = */
-    
     forkJoin({
       user1: this.userService.getUser(1).pipe(delay(2000)),
       user2: this.userService.getUser(2).pipe(delay(3000)),
       user3: this.userService.getUser(3).pipe(delay(6000)),
-      posts: this.postService.getPostsByParams('userId=1&userId=2&userId=3')
+      posts: this.postService.getPostsByParams('userId=1&userId=2&userId=3'),
     }).subscribe((data) => {
-      console.log(data);
-    })
+      this.user1 = {
+        name: data.user1.name,
+        count: this.getUserNPostCountWithE(data.user1.id, data.posts),
+      };
+      this.user2 = {
+        name: data.user2.name,
+        count: this.getUserNPostCountWithE(data.user2.id, data.posts),
+      };
+      this.user3 = {
+        name: data.user3.name,
+        count: this.getUserNPostCountWithE(data.user3.id, data.posts),
+      };
+    });
   }
 
+  getUserNPostCountWithE(id: number, posts: Post[]): number {
+    let postCounterE = posts.filter(
+      (post) => post.userId === id && post.title.startsWith('e', 0)
+    );
+    return postCounterE.length;
+  }
 }
--- http-rxjs/src/app/services/user.service.ts
+++ http-rxjs/src/app/services/user.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private API: string = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(`${this.API}`);
@@ -28,6 +31,4 @@ export class UserService {
   getUsersByEmail(username: string): Observable<User[]> {
     return this.httpClient.get<User[]>(`${this.API}?email=${username}`);
   }
-
-  
 }
