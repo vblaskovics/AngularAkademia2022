72bbd8d315f2ade5665764c6833a3d115e1c6fc6
vblaskovics <viktor.blaskovics@gmail.com>
2022-09-29 13:41:25 +0200

    Add rxjs learning project

--- rxjs-chat/src/app/app.component.ts
+++ rxjs-chat/src/app/app.component.ts
@@ -18,25 +18,21 @@ export class AppComponent {
       console.log('messages:', messages);
     })
 
-    /*this.messageService.updates$.next((messages: Message[]) => {
-      let msg1:Message = new Message({text:'Hello world 1'});
-      messages.push(msg1);
-      return messages;
-    });
-    this.messageService.updates$.next((messages: Message[]) => {
-      let msg2:Message = new Message({text:'Hello world 2'});
-      messages.push(msg2);
-      return messages;
-    });
-    this.messageService.updates$.next((messages: Message[]) => {
-      let msg3:Message = new Message({text:'Hello world 1'});
-      messages.push(msg3);
-      return messages;
-    });*/
-
     this.messageService.create$.next(new Message({text:'Hello world 1'}));
-    this.messageService.create$.next(new Message({text:'Hello world 2'}));
+    this.messageService.create$.next(new Message({text:'Hello world 2', sentAt: new Date("2022-09-01")}));
     this.messageService.create$.next(new Message({text:'Hello world 3'}));
+
+    this.messageService.createMessages$.next([
+      new Message({text:'Hello fuck 1'}),
+      new Message({text:'Hello fuck 2'}),
+      new Message({text:'Hello fuck 3', sentAt: new Date("2022-09-03")})
+    ]);
+    
+    this.messageService.censor$.next(['fuck', 'f#&k']);
+    
+    this.messageService.readOldMessages$.next(new Date("2022-09-28"));
+
+    this.messageService.create$.next(new Message({text:'Hello world 4'}));
 
   }
 }
--- rxjs-chat/src/app/message/message.service.ts
+++ rxjs-chat/src/app/message/message.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { filter, map, Observable, scan, Subject } from 'rxjs';
+import { filter, map, Observable, scan, Subject, tap } from 'rxjs';
 import { Thread } from '../thread/thread.model';
 import { User } from '../user/user.model';
 import { Message } from './message.model';
@@ -17,23 +17,68 @@ export class MessageService {
 
   messages$: Observable<Message[]>;
 
-  updates$: Subject<any> = new Subject<any>();
+  updates$: Subject<IMessageOperator> = new Subject<IMessageOperator>();
 
-  create$: Subject<Message> = new Subject<Message>()
+  create$: Subject<Message> = new Subject<Message>();
+
+  createMessages$: Subject<Message[]> = new Subject<Message[]>();
+
+  censor$: Subject<string[]> = new Subject<string[]>();
+
+  // 2. feladat
+  readOldMessages$ = new Subject<Date>();
+
+  // 3. feladat
+  messageCounter$ = new Observable<number>();
 
   constructor() {
 
     this.messages$ = this.updates$
-      .pipe(scan((messages: Message[], operation:IMessageOperator) => {
+      .pipe(scan((messages: Message[], operation: IMessageOperator) => {
         return operation(messages);
       }, []));
 
-    this.create$.pipe(map((message: Message)=> {
-      return (messages:Message[]) => {
-        messages.push(message);
+    this.create$.pipe(map((message: Message) => {
+      return (messages: Message[]) => {
+        return messages.concat(message);
+      }
+    })).subscribe(this.updates$);
+
+    this.createMessages$.pipe(map((newMessages: Message[]) => {
+      return (messages: Message[]) => messages.concat(newMessages)
+    })).subscribe(this.updates$);
+
+    this.censor$.pipe(map((options: string[]) => {
+      return (messages: Message[]) => {
+        messages.map((m) => {
+          m.text = m.text.replace(options[0], options[1]);
+          return m;
+        })
         return messages;
       }
     })).subscribe(this.updates$);
+
+    // 1. feladat 
+    this.updates$.subscribe(() => {
+      console.log("update történt");
+    });
+
+    // 2. feladat
+    this.readOldMessages$.pipe(
+      tap(() => console.log('readOldMessages$ event')),
+      map((d: Date): IMessageOperator => {
+        return (msgs: Message[]) => msgs.map((m) => {
+          if (m.sentAt < d) {
+            m.isRead = true;
+          }
+          return m
+        })
+      })).subscribe(this.updates$);
+
+    // 3. feladat
+    this.messageCounter$ = this.messages$.pipe(map((msgs: Message[]) => msgs.length));
+    this.messageCounter$.subscribe((count) => console.log("Messages count", count));
+
 
   }
 
