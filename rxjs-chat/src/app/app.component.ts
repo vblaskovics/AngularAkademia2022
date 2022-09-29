6b1cfae01e0f3336e22eff5e590d8934cfd2cde5
BeZol22 <zoltan.beke89@gmail.com>
2022-09-29 13:19:37 +0200

    more streams added

--- rxjs-chat/src/app/app.component.ts
+++ rxjs-chat/src/app/app.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { Message } from './message/message.model';
+import { MessageService } from './message/message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Rxjs Chat App';
 
-  constructor() { }
+  constructor(private messageService: MessageService) {
+    let msg1: Message = new Message({ text: 'Hello fuck 1' });
+    let msg2: Message = new Message({ text: 'Hello fuck 2' });
+    let msg3: Message = new Message({ text: 'Hello fuck 3' });
+
+    let msg4: Message = new Message({
+      text: 'Hello fuck 4',
+      sentAt: new Date(2023, 5, 14),
+    });
+    let msg5: Message = new Message({
+      text: 'Hello fuck 5',
+      sentAt: new Date(2020, 1, 25),
+    });
+
+    this.messageService.messages$.subscribe((messages: Message[]) => {
+      console.log('messages:', messages);
+    });
+
+    // this.messageService.updates$.next((messages: Message[]) => {
+    //   messages.push(msg1);
+    //   return messages;
+    // });
+    // this.messageService.updates$.next((messages: Message[]) => {
+    //   messages.push(msg2);
+    //   return messages;
+    // });
+    // this.messageService.updates$.next((messages: Message[]) => {
+    //   messages.push(msg3);
+    //   return messages;
+    // });
+
+    this.messageService.create$.next(msg1);
+    this.messageService.create$.next(msg2);
+    this.messageService.create$.next(msg3);
+
+    this.messageService.create$.next(msg4);
+    this.messageService.create$.next(msg5);
+    this.messageService.dates$.next(new Date());
+
+    this.messageService.createMessages$.next([msg1, msg2, msg3]);
+
+    this.messageService.censor$.next(['fuck', 'f#$k']);
+  }
 }
---
+++ rxjs-chat/src/app/message/message.service.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import { now } from 'moment';
+import { filter, map, Observable, scan, Subject } from 'rxjs';
+import { Thread } from '../thread/thread.model';
+import { User } from '../user/user.model';
+import { Message } from './message.model';
+
+interface IMessageOperator extends Function {
+  (messages: Message[]): Message[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  newMessage$: Subject<Message> = new Subject<Message>();
+
+  messages$: Observable<Message[]>;
+
+  updates$: Subject<IMessageOperator> = new Subject<IMessageOperator>();
+
+  create$: Subject<Message> = new Subject<Message>();
+  createMessages$: Subject<Message[]> = new Subject<Message[]>();
+
+  censor$: Subject<string[]> = new Subject<string[]>();
+
+  dates$: Subject<Date> = new Subject<Date>();
+  messageNumber$: Observable<number> = new Observable<number>();
+
+  constructor() {
+    this.messages$ = this.updates$.pipe(
+      scan((messages: Message[], operation: IMessageOperator) => {
+        return operation(messages);
+      }, [])
+    );
+
+    this.messageNumber$ = this.messages$.pipe(
+      map((messages) => messages.length)
+    );
+
+    this.messageNumber$.subscribe((msg) => console.log(msg));
+
+    this.updates$.subscribe(() => console.log('update happened'));
+
+    this.dates$
+      .pipe(
+        map((date: Date) => {
+          return (messages: Message[]) => {
+            return messages.map((msg) => {
+              if (msg.sentAt < date) {
+                msg.isRead = true;
+              }
+              return msg;
+            });
+            // return messages;
+          };
+        })
+      )
+      .subscribe(this.updates$);
+
+    this.create$
+      .pipe(
+        map((message: Message) => {
+          return (messages: Message[]) => {
+            messages.push(message);
+            return messages;
+          };
+        })
+      )
+      .subscribe(this.updates$);
+
+    this.createMessages$
+      .pipe(
+        map((newMessages: Message[]) => {
+          return (messages: Message[]) => messages.concat(newMessages);
+        })
+      )
+      .subscribe(this.updates$);
+
+    this.censor$
+      .pipe(
+        map((options: string[]) => {
+          return (messages: Message[]) => {
+            messages.map((msg) => {
+              msg.text = msg.text.replace(options[0], options[1]);
+              return msg;
+            });
+            return messages;
+          };
+        })
+      )
+      .subscribe(this.updates$);
+  }
+
+  addMessage(message: Message) {
+    this.newMessage$.next(message);
+  }
+
+  messagesForThreadUser(thread: Thread, user: User) {
+    return this.newMessage$.pipe(
+      filter(
+        (message: Message) =>
+          message.thread.id === thread.id && message.author.id !== user.id
+      )
+    );
+  }
+}
