e88059262b26b29d23ef78b921408351586b9648
Freetze <babos1221@gmail.com>
2022-10-19 12:18:58 +0200

    matTableModule is not working properly

--- todo-manager-webapp/src/app/material/material.module.ts
+++ todo-manager-webapp/src/app/material/material.module.ts
@@ -3,8 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
+// import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
@@ -15,7 +15,7 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule,
     MatButtonToggleModule,
     MatCardModule,
-    MatTableModule,
+    // MatTableModule,
   ],
   exports: [
     MatButtonModule,
@@ -24,7 +24,7 @@ import { MatCardModule } from '@angular/material/card';
     MatCardModule,
     MatButtonToggleModule,
     MatCardModule,
-    MatTableModule,
+    // MatTableModule,
   ],
 })
 export class MaterialModule {}
