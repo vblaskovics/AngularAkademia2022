0ba63bfc01af60e0302c0b7032ce4550f7ae0b2c
floratoth <a.flora95@gmail.com>
2022-10-27 09:18:14 +0200

    Change actions

---
+++ ngrx-tutorial/src/app/state/books.actions.ts
@@ -0,0 +1,17 @@
+import { createAction, props } from '@ngrx/store';
+import { Book } from '../book-list/books.model';
+
+export const addBook = createAction(
+  '[Book List] Add Book',
+  props<{ bookId: string }>()
+);
+
+export const removeBook = createAction(
+  '[Book Collection] Remove Book',
+  props<{ bookId: string }>()
+);
+
+export const retreivedBookList = createAction(
+  '[Book List/API]',
+  props<{ books: ReadonlyArray<Book> }>()
+);
